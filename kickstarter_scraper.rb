7bc24cbe2b20fca6a093097d38651718f757313a
Candice Parker <lexus2424@gmail.com>
2019-06-19 13:51:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,4 +15,4 @@ create_project_hash
 
 #projects: kickstarter.css("li.project.grid_4")project
 #title project.css("h2.bbcard_name strong a").text
-#
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
