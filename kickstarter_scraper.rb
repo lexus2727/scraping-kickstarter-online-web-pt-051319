d937ebb1c2ce98bd8e728041203c44b8f77aa66a
Candice Parker <lexus2424@gmail.com>
2019-06-19 13:31:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,14 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
-end
+  #This just opens up a file and reds it into a local_variables
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
+  end
+create_project_hash
+
+
+#projects: kickstarter.css("li.project.grid_4")project
